f6c899334a4511c3bfa5900a236ed0072deeba6d
Hugo BARTHELEMY <barthelemyhugo@gmail.com>
2017-03-09 23:41:39 +0100

    with regex

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,23 @@ class Users::SessionsController < Devise::SessionsController
   def connect
     @encrypt = AesEncryptDecrypt.encrypt("bis")
     @decrypt = AesEncryptDecrypt.decrypt(params[:string])
+
+    # test format
+    if regex?
+      @coucou = "hello world"
+
+    end
   end
 
   private
+    def regex?
+
+      if @decrypt =~ /^\d\d\d\d\d\dH\d\d\d\d-\d\d\d\d\d\d$/
+        return true
+      else
+        return false
+      end
+    end
 
     def after_sign_in_path_for(resource)
       if !verifying_via_email? && resource.show_welcome_screen?
@@ -25,3 +39,5 @@ class Users::SessionsController < Devise::SessionsController
     end
 
 end
+
+
