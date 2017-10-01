6725344a7c29d5f2f00cf8540ac22eeac37447fa
hugo barthelemy <barthelemyhugo@gmail.com>
2017-10-01 16:59:27 +0200

    modif show of results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,16 +12,22 @@ class PagesController < ApplicationController
     @results = []
     file = File.expand_path('../../../../consul/app/assets/doc/results.csv', __FILE__)
     CSV.foreach(file, :headers => true) do |row|
-      @results.push({site: row[0],
-      win: row[21],
+      @results.push(project_ligne(row))
+    end
+  end
+
+  private
+
+  def project_ligne(row)
+    {site: row[0],
+      win: row[20],
       name: row[4],
       id: row[1].to_i,
       accepted: row[9],
       cost: row[7],
-      paper_vote: row[18],
+      paper_vote: row[17],
       electronic_vote: Budget::Ballot::Line.where(investment_id: row[1]).count,
-      total: row[20],
-      color: row[22]})
-    end
+      total: row[19],
+      color: row[21]}
   end
 end
